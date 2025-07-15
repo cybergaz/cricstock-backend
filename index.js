a5aead0943fdd12a78437e8598c42987723ed639
cybregaz <kkanttechy@gmail.com>
2025-07-15 20:30:08 +0530

    final deployment x2

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const io = initializeSocket(server);
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "*",
     methods: "GET,POST,PUT,DELETE,PATCH",
     credentials: true,
   })
