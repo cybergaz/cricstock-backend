3a2aeb1907880f65bb65f973b8f2b4bf89a16e89
cybregaz <kkanttechy@gmail.com>
2025-07-16 17:49:14 +0530

    deployment x12

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ app.use(
     origin: "https://www.cricstock11.com",
     methods: "GET,POST,PUT,DELETE,PATCH",
     credentials: true,
+    allowedHeaders: ["Content-type", "Authorization", "Set-Cookie"],
   })
 );
 app.use(
