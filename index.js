bd67eca3dad17e6240fffbb275d443e9b07b3c18
cybergaz <kkanttechy@gmail.com>
2025-07-17 00:34:34 +0530

    origin changes

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://www.cricstock11.com"],
+    origin: ["http://localhost:3000", "https://www.cricstock11.com", "https://cricket-frontend-nextjs-git-auth-flow-impl-cybergazs-projects.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     credentials: true,
     allowedHeaders: ["Content-type", "Authorization", "Set-Cookie"],
